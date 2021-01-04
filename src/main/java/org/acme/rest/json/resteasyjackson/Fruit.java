3e3200891ef5ed445a2f4b4309a9d9707b9538d4
Robert Dimitrov <robert_dimitrov@me.com>
2021-01-04 18:04:15 +0200

    Initial commit

---
+++ src/main/java/org/acme/rest/json/resteasyjackson/Fruit.java
@@ -0,0 +1,13 @@
+package org.acme.rest.json.resteasyjackson;
+
+public class Fruit {
+    public String name;
+    public String description;
+
+    public Fruit() {}
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
---
+++ src/main/java/org/acme/rest/json/resteasyjackson/Legume.java
@@ -0,0 +1,13 @@
+package org.acme.rest.json.resteasyjackson;
+
+public class Legume {
+    public String name;
+    public String description;
+
+    public Legume() {}
+
+    public Legume(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
